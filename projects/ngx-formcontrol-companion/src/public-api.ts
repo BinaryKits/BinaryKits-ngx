65acf30870ac514974e99d9fffd6fbc396133dbe
Ruan Yiping <yiping.ruan@outlook.com>
2021-09-17 16:26:00 +0800

    Added ngx-formcontrol-companion

---
+++ projects/ngx-formcontrol-companion/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of ngx-formcontrol-companion
+ */
+
+export * from './lib/ngx-formcontrol-companion.service';
+export * from './lib/ngx-formcontrol-companion.component';
+export * from './lib/ngx-formcontrol-companion.module';
