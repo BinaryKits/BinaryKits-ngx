403e407fe6d4e3457ccbfdcd6a41885bdfd4b23e
Ruan Yiping <yiping.ruan@outlook.com>
2021-12-04 20:05:19 +0800

    fix datepicker value keeper logic

--- projects/binarykits/ngx-mat-datepicker-simple-formatting/src/lib/ngx-mat-datepicker-simple-formatting.directive.ts
+++ projects/binarykits/ngx-mat-datepicker-simple-formatting/src/lib/ngx-mat-datepicker-simple-formatting.directive.ts
@@ -10,10 +10,11 @@ import { Subscription } from 'rxjs';
   selector: 'input[bkMatDatepickerSimpleFormatting]'
 })
 export class NgxMatDatepickerSimpleFormattingDirective implements OnInit, OnDestroy {
-  @Input() bkMatDatepickerSimpleFormatting!: AbstractControl;
+  @Input() bkMatDatepickerSimpleFormatting!: AbstractControl
   @Input() sourceFormat = "YYYY-MM-DD"
   input: HTMLInputElement
   subscriptions: Subscription[] = []
+  matDatePickerFormat = "MM/DD/YYYY"
 
   constructor(el: ElementRef<HTMLInputElement>) {
     dayjs.extend(customParseFormat)
@@ -21,6 +22,7 @@ export class NgxMatDatepickerSimpleFormattingDirective implements OnInit, OnDest
   }
 
   ngOnInit(): void {
+    this.syncSourceChanges(this.sourceFormControl.value)  // Apply inital value
     const s = this.sourceFormControl.valueChanges.subscribe(value => this.syncSourceChanges(value))
     this.subscriptions.push(s)
   }
@@ -33,28 +35,19 @@ export class NgxMatDatepickerSimpleFormattingDirective implements OnInit, OnDest
   @HostListener("dateInput", ["$event"])
   dateInput(event: MatDatepickerInputEvent<Date>): void {
     const newValue = dayjs(event.value).format(this.sourceFormat)
-
-    // Avoid infinite loop
-    if (newValue !== this.sourceFormControl.value) {
+    
+    if (newValue !== this.sourceFormControl.value) {  // Avoid infinite loop
       this.sourceFormControl.setValue(newValue)
     }
   }
 
   // Source formControl changes value => datepicker
   syncSourceChanges(value: string): void {
-    const newValueInDayjs = dayjs(value, this.sourceFormat)
-
-    if (newValueInDayjs.format(this.sourceFormat) !== value) { // Not a valid date string
-      return
+    const originalValue = dayjs(value, this.sourceFormat).format(this.matDatePickerFormat)
+    if (originalValue !== "Invalid Date") {
+      this.input.value = originalValue
+      this.input.dispatchEvent(new Event("input"))
     }
-
-    const newValue = newValueInDayjs.toDate().toLocaleDateString() // Value used by the mat-datepicker
-    if (this.input.value === newValue) {
-      return
-    }
-
-    this.input.value = newValue
-    this.input.dispatchEvent(new Event("input"))  // Trigger calendar UI update selected date
   }
 
   get sourceFormControl(): AbstractControl {
